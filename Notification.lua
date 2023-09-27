55f1b8e675e57b576462707f1306b901daa248ab
MakerScript <104077050+MakerScript@users.noreply.github.com>
2023-09-27 19:16:17 +0700

    Create Notification.lua

---
+++ Notification.lua
@@ -0,0 +1,169 @@
+local Notify = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Panel = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local notificationOn = Instance.new("ImageLabel")
+local notificationOn2 = Instance.new("ImageLabel")
+local TextTitle = Instance.new("TextLabel")
+local UICorner_2 = Instance.new("UICorner")
+local TextTitle_2 = Instance.new("TextLabel")
+local DropShadowHolder = Instance.new("Frame")
+local DropShadow = Instance.new("ImageLabel")
+
+-- Properties:
+
+Notify.Name = "Notify"
+Notify.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Notify.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Main.Name = "Main"
+Main.Parent = Notify
+Main.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+Main.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Main.BorderSizePixel = 0
+Main.Position = UDim2.new(0, 0, 0.775561094, 0)
+Main.Size = UDim2.new(0, 297, 0, 112)
+
+Panel.Name = "Panel"
+Panel.Parent = Main
+Panel.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+Panel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Panel.BorderSizePixel = 0
+Panel.Size = UDim2.new(0, 297, 0, 30)
+
+UICorner.CornerRadius = UDim.new(0, 6)
+UICorner.Parent = Panel
+
+notificationOn.Name = "notificationOn"
+notificationOn.Parent = Panel
+notificationOn.BackgroundTransparency = 1.000
+notificationOn.Position = UDim2.new(0.0269360263, 0, 0, 0)
+notificationOn.Size = UDim2.new(0, 32, 0, 30)
+notificationOn.Image = "rbxassetid://14550341879"
+notificationOn.ImageRectOffset = Vector2.new(152, 304)
+notificationOn.ImageRectSize = Vector2.new(36, 36)
+
+notificationOn2.Name = "notificationOn2"
+notificationOn2.Parent = Panel
+notificationOn2.BackgroundTransparency = 1.000
+notificationOn2.Position = UDim2.new(0.865319848, 0, 0, 0)
+notificationOn2.Size = UDim2.new(0, 32, 0, 30)
+notificationOn2.Image = "rbxassetid://14550341879"
+notificationOn2.ImageRectOffset = Vector2.new(152, 304)
+notificationOn2.ImageRectSize = Vector2.new(36, 36)
+
+TextTitle.Name = "TextTitle"
+TextTitle.Parent = Panel
+TextTitle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextTitle.BackgroundTransparency = 1.000
+TextTitle.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextTitle.BorderSizePixel = 0
+TextTitle.Position = UDim2.new(0.134680137, 0, 0, 0)
+TextTitle.Size = UDim2.new(0, 217, 0, 30)
+TextTitle.Font = Enum.Font.GothamMedium
+TextTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextTitle.TextScaled = true
+TextTitle.TextSize = 14.000
+TextTitle.TextWrapped = true
+
+UICorner_2.CornerRadius = UDim.new(0, 7)
+UICorner_2.Parent = Main
+
+TextTitle_2.Name = "TextTitle"
+TextTitle_2.Parent = Main
+TextTitle_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextTitle_2.BackgroundTransparency = 1.000
+TextTitle_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextTitle_2.BorderSizePixel = 0
+TextTitle_2.Position = UDim2.new(0, 0, 0.267857134, 0)
+TextTitle_2.Size = UDim2.new(0, 297, 0, 82)
+TextTitle_2.Font = Enum.Font.Gotham
+TextTitle_2.Text = "Im a text!"
+TextTitle_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextTitle_2.TextScaled = true
+TextTitle_2.TextSize = 14.000
+TextTitle_2.TextWrapped = true
+
+DropShadowHolder.Name = "DropShadowHolder"
+DropShadowHolder.Parent = Main
+DropShadowHolder.BackgroundTransparency = 1.000
+DropShadowHolder.BorderSizePixel = 0
+DropShadowHolder.Size = UDim2.new(1, 0, 1, 0)
+DropShadowHolder.ZIndex = 0
+
+DropShadow.Name = "DropShadow"
+DropShadow.Parent = DropShadowHolder
+DropShadow.AnchorPoint = Vector2.new(0.5, 0.5)
+DropShadow.BackgroundTransparency = 1.000
+DropShadow.BorderSizePixel = 0
+DropShadow.Position = UDim2.new(0.5, 0, 0.5, 0)
+DropShadow.Size = UDim2.new(1, 47, 1, 47)
+DropShadow.ZIndex = 0
+DropShadow.Image = "rbxassetid://6015897843"
+DropShadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
+DropShadow.ImageTransparency = 0.500
+DropShadow.ScaleType = Enum.ScaleType.Slice
+DropShadow.SliceCenter = Rect.new(49, 49, 450, 450)
+
+Main.Transparency = 1
+Panel.Transparency = 1
+notificationOn.ImageTransparency = 1
+notificationOn2.ImageTransparency = 1
+TextTitle.TextTransparency = 1
+TextTitle_2.TextTransparency = 1
+
+
+function Show()
+	game.TweenService:Create(Main, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		Transparency = 0
+	}):Play()
+	DropShadowHolder.Visible = true
+	game.TweenService:Create(Panel, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		Transparency = 0
+	}):Play()
+	game.TweenService:Create(notificationOn, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		ImageTransparency = 0
+	}):Play()
+	game.TweenService:Create(notificationOn2, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		ImageTransparency = 0
+	}):Play()
+	game.TweenService:Create(TextTitle, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		TextTransparency = 0
+	}):Play()
+	game.TweenService:Create(TextTitle_2, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		TextTransparency = 0
+	}):Play()
+end
+
+function Hide()
+	game.TweenService:Create(Main, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		Transparency = 1
+	}):Play()
+	DropShadowHolder.Visible = false
+	game.TweenService:Create(Panel, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		Transparency = 1
+	}):Play()
+	game.TweenService:Create(notificationOn, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		ImageTransparency = 1
+	}):Play()
+	game.TweenService:Create(notificationOn2, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		ImageTransparency = 1
+	}):Play()
+	game.TweenService:Create(TextTitle, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		TextTransparency = 1
+	}):Play()
+	game.TweenService:Create(TextTitle_2, TweenInfo.new(0.3, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+		TextTransparency = 1
+	}):Play()
+end
+
+
+function Notify(text, title, time)
+	Show()
+	TextTitle_2.Text = title
+	TextTitle.Text = text
+	wait(time)
+	Hide()
+	wait(1)
+	game.Players.LocalPlayer:WaitForChild("PlayerGui").Notify:Destroy()
+end
